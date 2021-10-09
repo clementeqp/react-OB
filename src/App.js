ebf752af00f8e7368d42e5a09586f8fd0dae45c6
Clementeqp <clementeqp@hotmail.com>
2021-10-09 08:09:30 +0200

    Session 7

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 //import Greeting from './components/pure/greeting';
 //import Greetingf from './components/pure/greetingF';
-//import TaskListComponent from './components/container/task_list';
+import TaskListComponent from './components/container/task_list';
 //import Ejemplo1 from './hooks/Ejemplo1';
 //import Ejemplo2 from './hooks/Ejemplo2';
 //import Ejemplo3 from './hooks/Ejemplo3';
 //import MiComponenteConContexto from './hooks/Ejemplo3';
 //import Ejemplo4 from './hooks/Ejemplo4';
-import GrettingStyled from './components/pure/greetingStyled';
+//import GrettingStyled from './components/pure/greetingStyled';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         {/*Componente propio Greting.jsdx y funcional F */}
         {/* <Greeting name='Clemen'></Greeting> */}
         {/* <Greetingf name="Clemente"></Greetingf> */}
-        {/* <TaskListComponent></TaskListComponent> */}
+        <TaskListComponent></TaskListComponent>
         {/* Ejemplo de uso de hooks */}
         {/* <Ejemplo1></Ejemplo1> */}
         {/* <Ejemplo2></Ejemplo2> */}
@@ -29,7 +29,7 @@ function App() {
             Contenido del props.children
           </h3>
         </Ejemplo4> */}
-        <GrettingStyled name="Clemen"></GrettingStyled>
+        {/* <GrettingStyled name="Clemen"></GrettingStyled> */}
       </header>
     </div>
   );
